7ef4784193ec8862222cc2852e5e0f9d1d053a4f
Andrea Prat <andr8ide@gmail.com>
2023-08-17 07:16:16 +0200

    Create toDosSlice with loadToDos and deleteToDo

---
+++ src/store/toDos/toDosSlice.ts
@@ -0,0 +1,34 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { ToDo } from "../../types";
+import { ToDoState } from "./types";
+
+const initialToDosState: ToDoState = {
+  toDos: [],
+};
+
+const toDosSlice = createSlice({
+  name: "toDos",
+  initialState: initialToDosState,
+  reducers: {
+    loadToDos: (
+      _currentToDosState,
+      action: PayloadAction<ToDo[]>,
+    ): ToDoState => ({
+      toDos: action.payload,
+    }),
+    deleteToDo: (
+      currentToDosState,
+      action: PayloadAction<number>,
+    ): ToDoState => ({
+      toDos: currentToDosState.toDos.filter(
+        (toDo) => toDo.id !== action.payload,
+      ),
+    }),
+  },
+});
+
+export const toDosReducer = toDosSlice.reducer;
+export const {
+  loadToDos: loadToDosActionCreator,
+  deleteToDo: deleteToDoActionCreator,
+} = toDosSlice.actions;
